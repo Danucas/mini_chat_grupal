0753f32d387584f5eab7ff9501e1818907ffda5a
Danucas <danrodcastillo1994@gmail.com>
2020-11-30 20:01:29 -0500

    adding collectstatic behavior to backend server

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@ python3 mini_chat/manage.py makemigrations api
 python3 mini_chat/manage.py migrate
 python3 mini_chat/manage.py migrate sessions
 python3 mini_chat/manage.py initadmin
-python3 mini_chat/manage.py collectstatic
+python3 mini_chat/manage.py collectstatic --noinput
 python3 mini_chat/manage.py generateschema --file openapi-schema.yml
 python3 mini_chat/manage.py runserver 0.0.0.0:8000
