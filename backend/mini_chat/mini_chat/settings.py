62dd7fc9e154f0e44f6be18d6f8d4f8a07fa261c
Danucas <danrodcastillo1994@gmail.com>
2020-11-30 20:06:59 -0500

    adding collectstatic behavior to backend server

--- backend/mini_chat/mini_chat/settings.py
+++ backend/mini_chat/mini_chat/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = '/usr/src/app/mini_chat/static/'
+STATIC_ROOT = '/usr/src/app/mini_chat/root/static/'
 
 STATIC_URL = '/api/static/'
 
