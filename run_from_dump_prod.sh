3a9c1168a05efa3468e2b458f7753a03e16292b1
Danucas <danrodcastillo1994@gmail.com>
2020-12-02 13:07:05 -0500

    port forward to db service

--- run_from_dump_prod.sh
+++ run_from_dump_prod.sh
@@ -5,7 +5,7 @@ sleep 5
 while ! nc -z -v -w5 0.0.0.0 5432; do		
     sleep 3
 done
-docker cp ./dump.sql mini_chat_grupal_db_1:/home/dump.back.sql
+docker cp ./dump.sql minichatgrupal_db_1:/home/dump.back.sql
 docker exec -it minichatgrupal_db_1 bash -c "dropdb -U mini_chat_dev mini_chat_dev_db"
 docker exec -it minichatgrupal_db_1 bash -c "createdb -U mini_chat_dev mini_chat_dev_db"
 docker exec -it minichatgrupal_db_1 bash -c "pg_restore -U mini_chat_dev -d mini_chat_dev_db /home/dump.back.sql"
